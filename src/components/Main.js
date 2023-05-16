3205fbead87d40f3f71a8ea0b0fb817b529d04b9
pksnowstorm <enguyen21@gmail.com>
2023-05-16 09:57:42 -0500

    frontend first commit

---
+++ src/components/Main.js
@@ -0,0 +1,59 @@
+import React, {useEffect, useState} from 'react'
+import {Routes, Route} from "react-router-dom"
+import Index from '../pages/Index'
+import Show from '../pages/Show'
+
+const Main = (props) => {
+    const [people, setPeople] = useState(null)
+  
+    // const URL = "http://localhost:4000/people/"
+    const URL = "https://person-app-backend-em9n.onrender.com/people"
+    
+    const getPeople = async () => {
+        const response = await fetch(URL);
+        const data = await response.json();
+        setPeople(data);
+    }
+
+    const createPeople = async(person) => {
+        await fetch(URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "Application/json",
+            },
+            body: JSON.stringify(person),
+        });
+        getPeople();
+    };
+
+    const updatePeople = async (person, id) => {
+        // make put request to create people
+        await fetch(URL + id, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "Application/json",
+          },
+          body: JSON.stringify(person),
+        });
+        getPeople();
+      }
+    
+      const deletePeople = async id => {
+        // make delete request to create people
+        await fetch(URL + id, {
+          method: "DELETE",
+        })
+        getPeople();
+      }
+    
+    useEffect(() => getPeople, []);
+
+    return (
+    <Routes>
+        <Route exact path="/" element={<Index people={people} createPeople={createPeople} />} />
+        <Route path="/people/:id" element={<Show people={people} updatePeople={updatePeople} deletePeople={deletePeople} />} />
+    </Routes>
+  )
+}
+
+export default Main
